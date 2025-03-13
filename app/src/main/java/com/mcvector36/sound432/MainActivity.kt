5a6c5cdaa4936ff34e6af9ea9f8e2b809d5a07cd
mcvector36 <constantinmihai804@gmail.com>
2025-03-13 15:33:40 +0200

    Refactor: Migrate to AppCompat and Material Components
    - Remove Jetpack Compose dependencies and related code.
    - Delete Color, Theme, and Type Compose files.
    - Replace Compose UI with AppCompat and Material Components.
    - Add a simple Material Button with a Toast message.
    - Update build.gradle.kts to reflect the change in libraries.
    - Update the manifest file to use `Theme.Material3.DayNight.NoActionBar`.
    - Remove  `android:requestLegacyExternalStorage="true"` from manifest.
    - Update themes.xml to utilize Material Design theming.

--- app/src/main/java/com/mcvector36/sound432/MainActivity.kt
+++ app/src/main/java/com/mcvector36/sound432/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.mcvector36.sound432
 
+
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myButton: MaterialButton = findViewById(R.id.myButton)
+        val myButton: MaterialButton = findViewById(R.id.imageButton)
         myButton.setOnClickListener {
-            Toast.makeText(this, "Buton apăsat!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Buton apăsat!", Toast.LENGTH_LONG).show()
         }
     }
 }
